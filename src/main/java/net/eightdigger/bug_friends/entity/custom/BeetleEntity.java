edde4942f9742bd5d62877cd98bb5d306db31fd2
eightdigger <gid.slay@gmail.com>
2024-03-10 20:53:59 -0400

    Im winning bich

--- src/main/java/net/eightdigger/bug_friends/entity/custom/BeetleEntity.java
+++ src/main/java/net/eightdigger/bug_friends/entity/custom/BeetleEntity.java
@@ -1,5 +1,6 @@
 package net.eightdigger.bug_friends.entity.custom;
 
+import com.google.common.collect.Sets;
 import net.eightdigger.bug_friends.entity.ModEntities;
 import net.eightdigger.bug_friends.entity.ai.BeetleAttackGoal;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -8,6 +9,8 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -15,17 +18,19 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Set;
 
 
-public class BeetleEntity extends Animal {
+public class BeetleEntity extends TamableAnimal{
     private static final EntityDataAccessor<Boolean> ATTACKING =
         SynchedEntityData.defineId(BeetleEntity.class, EntityDataSerializers.BOOLEAN);
-    public BeetleEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
+    public BeetleEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
@@ -89,17 +94,22 @@ public class BeetleEntity extends Animal {
 
     @Override
     protected void registerGoals() {
+
         this.goalSelector.addGoal(0, new FloatGoal(this));
 
+        this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
         this.goalSelector.addGoal(1, new BeetleAttackGoal(this, 1.0D, true));
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
         this.goalSelector.addGoal(1, new BreedGoal(this, 1D));
+        this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, 8.0F));
 
         this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1D));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.3D, Ingredient.of(Items.GLOW_BERRIES), false));
         this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
 
         this.goalSelector.addGoal(3, new FollowParentGoal(this, 1.2D));
+        this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1.0D, 5.0F, 1.0F, false));
+
 
     }
 
@@ -119,7 +129,7 @@ public class BeetleEntity extends Animal {
 
     @Override
     public boolean isFood(ItemStack pStack) {
-        return pStack.is(Items.GLOW_BERRIES);
+        return pStack.is(Items.SWEET_BERRIES);
     }
 
     @Nullable
@@ -129,5 +139,38 @@ public class BeetleEntity extends Animal {
     }
 
     //tameable entity
+    private static final Set<Item> TAME_FOOD = Sets.newHashSet(Items.GLOW_BERRIES);
 
+    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        if (!this.isTame() && TAME_FOOD.contains(itemstack.getItem())) {
+            if (!pPlayer.getAbilities().instabuild) {
+                itemstack.shrink(1);
+            }
+
+            if (!this.isSilent()) {
+                this.level().playSound((Player) null, this.getX(), this.getY(), this.getZ(), SoundEvents.PARROT_EAT, this.getSoundSource(), 1.0F, 1.0F + (this.random.nextFloat() - this.random.nextFloat()) * 0.2F);
+            }
+
+            if (!this.level().isClientSide) {
+                if (this.random.nextInt(10) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, pPlayer)) {
+                    this.tame(pPlayer);
+                    this.level().broadcastEntityEvent(this, (byte) 7);
+                } else {
+                    this.level().broadcastEntityEvent(this, (byte) 6);
+                }
+            }
+
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+
+        } else if (this.isTame() && this.isOwnedBy(pPlayer)) {
+            if (!this.level().isClientSide) {
+                this.setOrderedToSit(!this.isOrderedToSit());
+            }
+
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+        } else {
+            return super.mobInteract(pPlayer, pHand);
+        }
+    }
 }
